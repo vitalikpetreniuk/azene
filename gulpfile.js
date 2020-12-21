88b5584ddf65b858348135436e13238c38284e6c
vitalikpetreniuk <petreniuk.ua@gmail.com>
2020-12-21 18:05:24 +0200

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,7 @@ let paths = {
 
 	scripts: {
 		src: [
-			//'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
-			baseDir + '/js/owl.carousel.min.js',
+			'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
 			baseDir + '/js/app.js' // app.js. Always at the end
 		],
 		dest: baseDir + '/js',
